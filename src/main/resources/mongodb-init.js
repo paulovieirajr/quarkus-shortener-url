863a57216f68bbc2bace03811f4487078f12ca43
Paulo Vieira <paulovieiradev@outlook.com>
2025-05-24 19:54:55 -0300

    refactor: Improvement on script for create index

--- src/main/resources/mongodb-init.js
+++ src/main/resources/mongodb-init.js
@@ -1,8 +1,8 @@
 db = db.getSiblingDB('shortener');
 
 db.createUser({
-    user: "java",
-    pwd: "mongodb",
+    user: "admin",
+    pwd: "admin",
     roles: [
         {
             role: "readWrite",
@@ -18,5 +18,20 @@ db.ShortenedUrl.createIndex(
     {
         "expireAfterSeconds": 0,
         "name": "ttl_index"
+    }
+);
+
+db.ShortenedUrl.createIndex(
+    { "shortCode": 1 },
+    {
+        "unique": true,
+        "name": "shortcode_unique_index"
+    }
+);
+
+db.ShortenedUrl.createIndex(
+    { "originalUrl": 1 },
+    {
+        "name": "original_url_index"
     }
 );
